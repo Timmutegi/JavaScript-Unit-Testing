82d65899a23c47ecc364bea00a801079e4b2eb68
Timmutegi <mutegi.timothy538@gmail.com>
2022-02-23 15:58:26 +0200

    Added tests for asynchronous data

--- functions.test.js
+++ functions.test.js
@@ -33,19 +33,19 @@ test('user should be Timothy Mbaka', () => {
 // Working with async data
 
 // Promise
-// test('User fetched name should be Leanne Graham', () => {
-//   expect.assertions(1);
-//   return functions.fetchUser().then(data => {
-//     expect(data.name).toEqual('Leanne Graham');
-//   });
-// });
+test('User fetched name should be Leanne Graham', () => {
+  expect.assertions(1);
+  return functions.fetchUser().then(data => {
+    expect(data.name).toEqual('Leanne Graham');
+  });
+});
 
 // Async Await
-// test('User fetched name should be Leanne Graham', async () => {
-//     expect.assertions(1);
-//     const data = await functions.fetchUser();
-//     expect(data.name).toEqual('Leanne Graham');
-// });
+test('User fetched name should be Leanne Graham', async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
+});
 
 describe('status endpoint',  () => {
     test("fetch user", (done) => {
